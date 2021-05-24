a820c7e108e8b8c67ff135a1364c21ff8617e3bb
Julius Lee <7julius.lee@gmail.com>
2021-05-23 19:12:19 -0700

    added playList and 2 more songs

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,19 @@
 import './App.css';
 import styled from 'styled-components';
 import { useRef, useState, useEffect } from 'react';
-import { ReactComponent as PlaySVG } from './play-arrow.svg';
+import { ReactComponent as PlaySVG } from '../assets/play-arrow.svg';
+import { ReactComponent as NextSVG } from '../assets/next.svg';
 
 const AudioElement = styled.div`
      {
     }
 `;
 
-// const ButtonElement = styled.button`
-//      {
-//         display: block;
-//         height: 50px;
-//         width: 100px;
-//         margin: auto;
-//     }
-// `;
+const playList = [
+    'Atmosphere.mp3',
+    'Destroy-capitalism-for-christmas.mp3',
+    'music.mp3'
+];
 
 const App = () => {
     const audioElement = useRef();
@@ -23,6 +21,8 @@ const App = () => {
     const intervalRef = useRef(0);
     const [title, setTitle] = useState('');
     const [trackProgress, setTrackProgress] = useState(0);
+    const [trackIndex, setTrackIndex] = useState(0);
+    const [isPlaying, setIsPlaying] = useState(false);
 
     const togglePlay = () => {
         console.log(audioElement);
@@ -31,22 +31,53 @@ const App = () => {
             clearInterval(intervalRef.current);
 
             intervalRef.current = setInterval(() => {
-                setTrackProgress(audioElement.current.currentTime);
+                setTrackProgress(
+                    (audioElement.current.currentTime /
+                        audioElement.current.duration) *
+                        100
+                );
             }, [1000]);
+            setIsPlaying(true);
         } else {
             audioElement.current.pause();
+            setIsPlaying(false);
         }
     };
 
     useEffect(() => {
         setTitle(audioElement.current.src);
-        setTrackProgress(audioElement.current.currentTime);
-    }, []);
+        setTrackProgress(
+            (audioElement.current.currentTime / audioElement.current.duration) *
+                100
+        );
+        setIsPlaying(true);
+    }, [trackIndex]);
+
+    useEffect(
+        () => () => {
+            console.log(`isplaying useEffect ran`);
+            intervalRef.current = setInterval(() => {
+                setTrackProgress(
+                    (audioElement.current.currentTime /
+                        audioElement.current.duration) *
+                        100
+                );
+            }, [1000]);
+        },
+        [isPlaying]
+    );
 
     const rangeSet = (e) => {
         audioElement.current.currentTime = Math.floor(
             (seekbar.current.value / 100) * audioElement.current.duration
         );
+    };
+
+    const next = () => {
+        if (trackIndex === playList.length - 1) {
+            return setTrackIndex(0);
+        }
+        setTrackIndex((track) => track + 1);
     };
 
     return (
@@ -57,15 +88,16 @@ const App = () => {
                 <audio
                     ref={audioElement}
                     controls
-                    src="./assets/music.mp3"
+                    src={`./assets/${playList[trackIndex]}`}
                     type="audio/mpeg"
+                    autoPlay={true}
                 ></audio>
-                {/* <ButtonElement onClick={togglePlay}>Play Pause</ButtonElement> */}
-                <PlaySVG
-                    className="play-btn"
-                    width="50px"
-                    onClick={togglePlay}
-                />
+                <button onClick={togglePlay}>
+                    <PlaySVG className="play-btn" width="50px" />
+                </button>
+                <button onClick={next}>
+                    <NextSVG className="next-btn" width="50px" />
+                </button>
                 <input
                     ref={seekbar}
                     type="range"
@@ -74,7 +106,6 @@ const App = () => {
                     min="0"
                     step="0.25"
                     max="100"
-                    defaultValue="0"
                     value={trackProgress}
                     onChange={rangeSet}
                 ></input>
